8272ae71e517b680eda95e194953188941de7786
larspmayrand <mayrand.lars@Gmail.com>
2018-08-28 18:52:50 -0700

    added favicon, index of photo, and 50% opacity backgrounds for text fields

--- main.js
+++ main.js
@@ -45,38 +45,43 @@ var titles =
     "IMG_2726-1.jpg",
     "pier beauty",
     "IMG_0905-1.jpg",
-    "pelicanrock",
+    "pelican rock",
     "corbett house",
     "the sculptor"];
 
 var metadata = 
-    ["1/26/15 10:24:18 PM<br>30.0 sec at f/8.0, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/6/15 6:36 PM<br>1/100 sec at f/5.6, ISO 400<br>250mm (EF-S55-250mm f/4-5.6 IS II)",
-    "4/19/15 7:01 AM<br>1/320 sec at f/13, ISO 100<br>250mm (EF-S55-250mm f/4-5.6 IS II)", 
-    "11/6/14 9:54 PM<br>1/60 sec at f/13, ISO 100<br>250mm (EF-S55-250mm f/4-5.6 IS II)",
-    "8/21/14 8:50 AM<br>1/250 sec at f/20, ISO 100<br> 55mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "12/18/14 3:26 PM<br>1/250 sec at f/14, ISO 100<br> 109mm (EF-S55-250mm f/4-5.6 IS II)",
-    "4/22/15 7:12 AM<br>1/4 sec at f/16, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "4/22/15 6:44 AM<br>1/15 sec at f/13, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "10/26/14 12:29 PM<br>1/80 sec at f/11, ISO 100<br>36mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/8/15 5:31 PM<br>1/20 sec at f/22, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "8/11/14 8:07 PM<br>1/2 sec at f/22, ISO 100<br>21mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/16/15 6:53 PM<br>1.3 sec at f/11, ISO 200<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/14/15 6:52 PM<br>30.0 sec at f/14, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/8/15 5:53 PM<br>1/100 sec at f/11, ISO 200<br>24mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/3/15 6:57 PM<br>30.0 sec at f/10, ISO 400<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "2/25/17 6:57 PM<br>1/5 sec at f/22, ISO 100<br>18mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "8/11/14 8:25 PM<br>1.3 sec at f/22, ISO 100<br>28mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "6/7/16 6:02 PM<br>1/160 sec at f/5.6, ISO 100<br>43mm (EF-S18-55mm f/3.5-5.6 IS II)",
-    "1/6/15 5:51 PM<br>1/640 sec at f/5.6, ISO 100<br>250mm (EF-S55-250mm f/4-5.6 IS II)", 
-    "10/8/14 2:04 AM<br>12.0 sec at f/7.1, ISO 400<br>24mm (EF-S18-55mm f/3.5-5.6 IS II)",
-     "8/5/14 2:31 PM<br>1/80 sec at f/5.6, ISO 400<br>24mm (EF-S18-55mm f/3.5-5.6 IS II)"];
+    [
+    "January 26, 2015 10:24 PM<br>30.0 sec at f/8.0 ISO 100",
+    "January 6, 2015 6:36 PM<br>1/100 sec at f/5.6 ISO 400",
+    "April 19, 2015 7:01 AM<br>1/320 sec at f/13 ISO 100",
+    "November 6, 2014 9:54 PM<br>1/60 sec at f/13 ISO 100",
+    "August 21, 2014 8:50 AM<br>1/250 sec at f/20 ISO 100",
+    "December 18, 2014 3:26 PM<br>1/250 sec at f/14 ISO 100",
+    "April 22, 2015 7:12 AM<br>1/4 sec at f/16 ISO 100",
+    "April 22, 2015 6:44 AM<br>1/15 sec at f/13, ISO 100",
+    "October 26, 2014 12:29 PM<br>1/80 sec at f/11 ISO 100",
+    "January 8, 2015 5:31 PM<br>1/20 sec at f/22 ISO 100",
+    "August 11, 2014 8:07 PM<br>1/2 sec at f/22 ISO 100",
+    "January 14, 2015 6:53 PM<br>1.3 sec at f/11 ISO 200",
+    "January 14, 2015 6:52 PM<br>30.0 sec at f/14 ISO 100",
+    "January 8 2015 5:53 PM<br>1/100 sec at f/11 ISO 200",
+    "January 3, 2015 6:57 PM<br>30.0 sec at f/10 ISO 400",
+    "February 25 2017 6:57 PM<br>1/5 sec at f/22, ISO 100",
+    "August 11, 2014 8:25 PM<br>1.3 sec at f/22 ISO 100",
+    "June 7, 2016 6:02 PM<br>1/160 sec at f/5.6 ISO 100",
+    "January 6, 2015 5:51 PM<br>1/640 sec at f/5.6 ISO 100", 
+    "October 8, 2014 2:04 AM<br>12.0 sec at f/7.1 ISO 400",
+    "August 5, 2014 2:31 PM<br>1/80 sec at f/5.6 ISO 400"];
 
 function update() {
     document.getElementById("photo").src = "photos/lo-rez/" + photos[current];
     document.getElementById("photo").src = "photos/hi-rez/" + photos[current];
     document.getElementById("title").innerHTML = titles[current];
     document.getElementById("metadata").innerHTML = metadata[current];
+    document.getElementById("title-box").innerHTML = titles[current];
+    document.getElementById("metadata-box").innerHTML = metadata[current];
+    document.getElementById("index").innerHTML = (current + 1) + "/" + photos.length;
+    document.getElementById("index-box").innerHTML = (current + 1) + "/" + photos.length;
 }
 
 function hideElements() {
@@ -84,8 +89,13 @@ function hideElements() {
     mouseTimer = null;
     document.getElementById("left-arrow").className = "arrows left_arrow hidden";
     document.getElementById("right-arrow").className = "arrows right_arrow hidden";
-    document.getElementById("title").className = "outline-text hidden";
-    document.getElementById("metadata").className = "outline-text hidden";
+    // document.getElementById("title").className = "text-block hidden";
+    document.getElementById("title").className = "outline-text title hidden";
+    document.getElementById("metadata").className = "outline-text metadata hidden";
+    document.getElementById("title-box").className = "text-box title hidden";
+    document.getElementById("metadata-box").className = "text-box metadata hidden";
+    document.getElementById("index").className = "outline-text index hidden";
+    document.getElementById("index-box").className = "text-box index hidden";
     setTimeout(function(){
         document.body.style.cursor = "none";
         cursorVisible = false;
@@ -96,20 +106,26 @@ function showElements() {
     console.log("showing elements!");
     document.getElementById("left-arrow").className = "arrows left_arrow";
     document.getElementById("right-arrow").className = "arrows right_arrow";
-    document.getElementById("title").className = "outline-text";
-    document.getElementById("metadata").className = "outline-text";
+    // document.getElementById("title").className = "text-block";
+    document.getElementById("title").className = "outline-text title";
+    document.getElementById("metadata").className = "outline-text metadata";
+    document.getElementById("title-box").className = "text-box title";
+    document.getElementById("metadata-box").className = "text-box metadata";
+    document.getElementById("index").className = "outline-text index";
+    document.getElementById("index-box").className = "text-box index";
     document.body.style.cursor = "default";
     cursorVisible = true;
 }
 
 function mouseMoved(){
+    showElements();
     if (mouseTimer) {
         window.clearTimeout(mouseTimer);
     }
     if (!cursorVisible) {
         showElements();
     }
-    mouseTimer = window.setTimeout(hideElements, 2000);
+    mouseTimer = window.setTimeout(hideElements, 3000);
 };
 
 // handling Internet Explorer stupidity with window.event
@@ -118,11 +134,10 @@ function checkKeycode(event) {
     // window.clearTimeout(mouseTimer);
     window.clearTimeout(mouseTimer);
     if (cursorVisible) {
-        showElements();
+        // showElements();
         mouseMoved();
         console.log("was the mouse moved?");
     }
-    
     var keyDownEvent = event || window.event,
         keycode = (keyDownEvent.which) ? keyDownEvent.which : keyDownEvent.keyCode;
     if (keycode == 37) { // left arrow
@@ -145,11 +160,9 @@ function next() {
 
 update();
 
-showElements();
-
 mouseMoved();
 
-window.setTimeout(hideBanner, 2000);
+window.setTimeout(hideBanner, 3000);
 
 function hideBanner() {
     document.getElementById("banner").className = "text-block banner hidden";
